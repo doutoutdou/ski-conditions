6659f88724ac5c0b354f23f8a73044197762ca70
doudou <edouard.talma@gmail.com>
2023-11-18 11:35:02 -0500

    :sparkles: Improving sutton

--- sample/sutton.py
+++ sample/sutton.py
@@ -20,4 +20,14 @@ def parse_sutton():
             sutton_conditions.set_total_trails(trail_information[1])
             break
 
+    lifts = content.find("div", class_="icon_btn icon_lift")
+
+    for elt in lifts.parent:
+        # example : <h3>0/60</h3>
+        if str(elt).startswith('<h3>'):
+            lift_information = elt.get_text().strip().split("/")
+            sutton_conditions.set_opened_chair_lifts(lift_information[0])
+            sutton_conditions.set_total_chair_lifts(lift_information[1])
+            break
+
     return vars(sutton_conditions)
